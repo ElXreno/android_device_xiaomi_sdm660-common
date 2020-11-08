28d14e2aa95b09cf97d3cd01e3be4e7afd01b7d6
ElXreno <elxreno@gmail.com>
2020-11-08 18:33:22 +0300

    sdm660-common: lavender: rewrite configure_zram_parameters
    Copied from: https://github.com/LineageOS/android_device_xiaomi_sdm660-common/blob/lineage-18.0/rootdir/etc/init.qcom.post_boot.sh
    
    Signed-off-by: ElXreno <elxreno@gmail.com>

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -32,36 +32,26 @@
 function configure_zram_parameters() {
     MemTotalStr=`cat /proc/meminfo | grep MemTotal`
     MemTotal=${MemTotalStr:16:8}
-    
-    # Zram disk - 75% for Go devices.
+
+    low_ram=`getprop ro.config.low_ram`
+
+    # ZRAM disk - 75% for Go devices.
     # For 512MB Go device, size = 384MB, set same for Non-Go.
     # For 1GB Go device, size = 768MB, set same for Non-Go.
-    # For 2GB Go device, size = 1536MB, set same for Non-Go.
-    # For >2GB Non-Go devices, size = 50% of RAM size. Limit the size to 4GB.
+    # For >=2GB Non-Go devices, size = 50% of RAM size. Limit the size to 4GB.
     # And enable lz4 zram compression for Go targets.
 
     RamSizeGB=`echo "($MemTotal / 1048576 ) + 1" | bc`
-    if [ $RamSizeGB -le 2 ]; then
-        zRamSizeBytes=`echo "$RamSizeGB * 1024 * 1024 * 1024 * 3 / 4" | bc`
-        zRamSizeMB=`echo "$RamSizeGB * 1024 * 3 / 4" | bc`
-    else
-        zRamSizeBytes=`echo "$RamSizeGB * 1024 * 1024 * 1024 / 2" | bc`
-        zRamSizeMB=`echo "$RamSizeGB * 1024 / 2" | bc`
-    fi
-
-    # use MB avoid 32 bit overflow
-    if [ $zRamSizeMB -gt 4096 ]; then
+    zRamSizeBytes=`echo "$RamSizeGB * 1024 * 1024 * 1024 / 2" | bc`
+    if [ $zRamSizeBytes -gt 4294967296 ]; then
         zRamSizeBytes=4294967296
     fi
 
-    echo lz4 > /sys/block/zram0/comp_algorithm
+    if [ "$low_ram" == "true" ]; then
+        echo lz4 > /sys/block/zram0/comp_algorithm
+    fi
 
     if [ -f /sys/block/zram0/disksize ]; then
-        if [ -f /sys/block/zram0/use_dedup ]; then
-            echo 1 > /sys/block/zram0/use_dedup
-        fi
-        echo $zRamSizeBytes > /sys/block/zram0/disksize
-
         # ZRAM may use more memory than it saves if SLAB_STORE_USER
         # debug option is enabled.
         if [ -e /sys/kernel/slab/zs_handle ]; then
@@ -71,6 +61,19 @@ function configure_zram_parameters() {
             echo 0 > /sys/kernel/slab/zspage/store_user
         fi
 
+        # Enable memory deduplication if this feature available
+        if [ -f /sys/block/zram0/use_dedup ]; then
+            echo 1 > /sys/block/zram0/use_dedup
+        fi
+
+        # Set ZRAM disk size
+        if [ $MemTotal -le 524288 ]; then
+            echo 402653184 > /sys/block/zram0/disksize
+        elif [ $MemTotal -le 1048576 ]; then
+            echo 805306368 > /sys/block/zram0/disksize
+        else
+            echo $zRamSizeBytes > /sys/block/zram0/disksize
+        fi
         mkswap /dev/block/zram0
         swapon /dev/block/zram0 -p 32758
     fi
